1cdf52e4c238d9f9c1b64eed5cac5af99b0734e1
Yili Liu <yililiu7@gmail.com>
2020-12-17 17:29:30 -0500

    draw by lines

--- Skribbler.java
+++ Skribbler.java
@@ -15,34 +15,40 @@ public class Skribbler {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        final String API_KEY = getKey();
-
         System.out.println("Welcome to Skribbler");
-        System.out.println("Simply enter the word to be drawn or press enter to exit");
+        System.out.println("Simply enter the word (or image link) to be drawn and press enter to exit");
 
         try {
             String s = br.readLine();
             if (s.length() != 0) {
-                s = s.replaceAll(" ", "+");
-                String res = getHttpResponse(
-                        "https://pixabay.com/api/?key=" + API_KEY + "&q=" + s + "&image_type=photo");
-
-                LinkedList<String> imgUrls = parseJson(res, "webformatURL");
-
-                String urlToDraw = "";
-                BufferedImage img = null;
-                s = "r";
-                while (!imgUrls.isEmpty() && s.length() != 0) {
-                    if (s.equals("r")) {
-                        urlToDraw = imgUrls.pop();
-                        img = ImageIO.read(new URL(urlToDraw));
-                    }
-
-                    System.out.println("Url to draw: " + urlToDraw);
+                if (s.startsWith("https")) {
+                    BufferedImage img = ImageIO.read(new URL(s));
+                    System.out.println("Url to draw: " + s);
                     Thread.sleep(2000);
-                    drawByDots(img);
-
-                    s = br.readLine();
+                    
+                    drawByLines(img);
+                } else {
+                    final String API_KEY = getKey();
+                    s = s.replaceAll(" ", "+");
+                    String res = getHttpResponse("https://pixabay.com/api/?key=" + API_KEY + "&q=" + s + "&image_type=photo");
+    
+                    LinkedList<String> imgUrls = parseJson(res, "webformatURL");
+    
+                    String urlToDraw = "";
+                    BufferedImage img = null;
+                    s = "r";
+                    while (!imgUrls.isEmpty() && s.length() != 0) {
+                        if (s.equals("r")) {
+                            urlToDraw = imgUrls.pop();
+                            img = ImageIO.read(new URL(urlToDraw));
+                        }
+    
+                        System.out.println("Url to draw: " + urlToDraw);
+                        Thread.sleep(2000);
+                        drawByLines(img);
+    
+                        s = br.readLine();
+                    }
                 }
 
                 br.close();
@@ -58,13 +64,12 @@ public class Skribbler {
         }
     }
 
-    private static void drawByDots(BufferedImage img) {
+    private static void drawByLines(BufferedImage img) {
         final int WIDTH = img.getWidth();
         final int HEIGHT = img.getHeight();
         final int X_OFFSET = 500;
         final int Y_OFFSET = 280;
-        final int THRESHOLD = 70;
-        final int SIDE = 6;
+        final int SIDE = 4;
 
         int[][] toDot = new int[WIDTH / SIDE][HEIGHT / SIDE];
         int[] allRgb = new int[toDot.length * toDot[0].length];
@@ -88,64 +93,35 @@ public class Skribbler {
         Arrays.sort(allRgb);
 
         for (int i = 0; i < toDot.length; i++) {
+            int px = -1;
+            int py = -1;
             for (int j = 0; j < toDot[0].length; j++) {
-                if (toDot[i][j] < allRgb[allRgb.length / 30]) {
-                    if (!square(X_OFFSET + i * SIDE, Y_OFFSET + j * SIDE, SIDE)) {
-                        System.out.println("A robot error has occurred");
-                        return;
-                    }
-                    // for (int k = i * SIDE; k < i * SIDE + 2; k++) {
-                    // for (int l = j * SIDE; l < j * SIDE + 2; l++) {
-                    // if (!dot(X_OFFSET + k, Y_OFFSET + l)) {
-                    // System.out.println("A robot error has occurred");
-                    // return;
-                    // }
-                    // }
-                    // }
-                } else if (toDot[i][j] < allRgb[allRgb.length / 3]) {
-                    if (!dot(X_OFFSET + i * SIDE, Y_OFFSET + j * SIDE)) {
-                        System.out.println("A robot error has occurred");
-                        return;
+                if (toDot[i][j] < allRgb[allRgb.length / 3]) {
+                    if (px == -1) {
+                        px = X_OFFSET + i * SIDE;
+                        py = Y_OFFSET + j * SIDE;
+                    } else if (j == toDot[0].length - 1 || toDot[i][j + 1] >= allRgb[allRgb.length / 3]) {
+                        if (!line(px, py, X_OFFSET + i * SIDE, Y_OFFSET + j * SIDE)) {
+                            System.out.println("A robot error has occurred");
+                            return;
+                        }
+                        px = -1;
+                        py = -1;
                     }
                 }
             }
         }
 
-        System.out.println("Done drawing by dots");
+        System.out.println("Done drawing by lines");
     }
 
-    private static boolean square(int x, int y, int side) {
+    private static boolean line(int x1, int y1, int x2, int y2) {
         try {
             Robot robot = new Robot();
-            robot.mouseMove(x, y);
-            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-            for (int k = x; k < x + side; k++) {
-                for (int l = y; l < y + side; l++) {
-                    robot.mouseMove(k, l);
-                    Thread.sleep(2);
-                }
-            }
-            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-
-        } catch (AWTException e) {
-            System.out.println("Cannot create java robot");
-            e.printStackTrace();
-            return false;
-        } catch (InterruptedException e) {
-            System.out.println("A sleep error has occurred");
-            e.printStackTrace();
-            return false;
-        }
-
-        return true;
-    }
-
-    private static boolean dot(int x, int y) {
-        try {
-            Robot robot = new Robot();
-            robot.mouseMove(x, y);
+            robot.mouseMove(x1, y1);
             robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
             Thread.sleep(2);
+            robot.mouseMove(x2, y2);
             robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
             return true;
         } catch (AWTException e) {
@@ -158,7 +134,7 @@ public class Skribbler {
             return false;
         }
     }
-
+    
     private static LinkedList<String> parseJson(String res, String key) {
         LinkedList<String> values = new LinkedList<String>();
         while (true) {
